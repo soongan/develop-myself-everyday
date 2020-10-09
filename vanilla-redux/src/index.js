db91b0565ee37396c965005d553d71e16e6fce50
soongan <soongan.jeon@gmail.com>
2020-10-09 14:32:01 +0900

    이렇게 가짜 리듀서라도 넣어줘야 redux 는 에러를 뱉지 않음

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,10 +1,15 @@
+import {createStore} from 'redux';
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
+const reducer = () => {};
+const store = createStore(reducer);
 
+let count = 0;
 number.innerText = count;
+
 const updateText = () => {
     number.innerText = count;
 };
