aecc1f159b18917a0113308ff79359c6cde6bde7
soongan <soongan.jeon@gmail.com>
2020-10-09 14:55:15 +0900

    심플 리듀서 심플리덕스 심플 액션

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -12,10 +12,25 @@ const countModifier = (count = 0, action) => {
     if(action.type == "ADD") {
         console.log("they are telling me to add one");
         return count +1;
+    } else if(action.type == "MINUS") {
+        return count -1;
+    } else {
+        return count;
     }
-    return count;
 };
 
 const countStore = createStore(countModifier);
 countStore.dispatch({type:"ADD"});
+countStore.dispatch({type:"ADD"});
+countStore.dispatch({type:"ADD"});
+countStore.dispatch({type:"ADD"});
+countStore.dispatch({type:"ADD"});
+countStore.dispatch({type:"MINUS"});
+
 console.log(countStore.getState());
+
+/*
+1 createStore 로 data의 store를 만들고 (create하고)
+2 data의 modifier가 countModifier 라는 걸 store에 알려주고
+3 하고싶은 동작. message를 store에 보내면 됨 (send하는 방법은 dispatch)
+*/
