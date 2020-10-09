478523256fb0f5990fc8927ba7e3cae0214d7960
soongan <soongan.jeon@gmail.com>
2020-10-09 14:46:39 +0900

    심플 리덕스

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -6,12 +6,16 @@ const number = document.querySelector("span");
 
 
 
-const countModifier = (state = 0) => {
+const countModifier = (count = 0, action) => {
     // ...modify statess
-    console.log(state);
-    return state;
+    // console.log(count, action); 
+    if(action.type == "ADD") {
+        console.log("they are telling me to add one");
+        return count +1;
+    }
+    return count;
 };
 
 const countStore = createStore(countModifier);
-
+countStore.dispatch({type:"ADD"});
 console.log(countStore.getState());
