47b9235468a8ac17977a9e723912e32d7af6aa0d
soongan <soongan.jeon@gmail.com>
2020-10-11 09:15:29 +0900

    바닐라 리덕스

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,50 +1,30 @@
-import {createStore} from 'redux';
+import { createStore } from "redux";
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-number.innerText = 0;
-
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-const countModifier = (count = 0, action) => {
-    // ...modify states
-    switch (action.type) {
-        case ADD:
-            return count + 1;
-        case MINUS:
-            return count - 1;
-        default:
-            return count;
-    }
+const reducer = (state = [], action) => {
+  console.log(action);
+  switch (action.type) {
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
+  }
 };
 
-const countStore = createStore(countModifier);
+const store = createStore(reducer);
 
-const onChange = () => {
-    // console.log("there was a change on the store");
-    // console.log(countStore.getState());
-    number.innerText = countStore.getState();
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  store.dispatch({ type: ADD_TODO, text: toDo });
 };
 
-countStore.subscribe(onChange);
-
-const handleAdd = () => {
-    countStore.dispatch({type: ADD });
-};
-const handleMinus = () => {
-    countStore.dispatch({type: MINUS });
-};
-
-
-countStore.subscribe(onChange);
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
-/*
-1 createStore 로 data의 store를 만들고 (create하고)
-2 data의 modifier가 countModifier 라는 걸 store에 알려주고
-3 하고싶은 동작. message를 store에 보내면 됨 (send하는 방법은 dispatch)
-*/
+form.addEventListener("submit", onSubmit);
