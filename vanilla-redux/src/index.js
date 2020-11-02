51b8479d06c1584015a0c5003c0896b80551bdaa
soongan <soongan.jeon@gmail.com>
2020-11-02 23:33:42 +0900

    11-02-mon

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,73 +1,78 @@
-import { createStore } from "redux";
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
+ReactDOM.render(<App></App>, document.getElementById("root"));
+// import { createStore } from "redux";
+// const form = document.querySelector("form");
+// const input = document.querySelector("input");
+// const ul = document.querySelector("ul");
 
-const addToDo = text => {
-  return {
-    type: ADD_TODO,
-    text
-  };
-};
+// const ADD_TODO = "ADD_TODO";
+// const DELETE_TODO = "DELETE_TODO";
 
-const deleteToDo = id => {
-  return {
-    type: DELETE_TODO,
-    id
-  };
-};
+// const addToDo = text => {
+//   return {
+//     type: ADD_TODO,
+//     text
+//   };
+// };
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      const newToDoObj = { text: action.text, id: Date.now() };
-      return [newToDoObj, ...state];
-    case DELETE_TODO:
-      const cleaned = state.filter(toDo => toDo.id !== action.id);
-      return cleaned;
-    default:
-      return state;
-  }
-};
+// const deleteToDo = id => {
+//   return {
+//     type: DELETE_TODO,
+//     id
+//   };
+// };
 
-const store = createStore(reducer);
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case ADD_TODO:
+//       const newToDoObj = { text: action.text, id: Date.now() };
+//       return [newToDoObj, ...state];
+//     case DELETE_TODO:
+//       const cleaned = state.filter(toDo => toDo.id !== action.id);
+//       return cleaned;
+//     default:
+//       return state;
+//   }
+// };
 
-store.subscribe(() => console.log(store.getState()));
+// const store = createStore(reducer);
 
-const dispatchAddToDo = text => {
-  store.dispatch(addToDo(text));
-};
+// store.subscribe(() => console.log(store.getState()));
 
-const dispatchDeleteToDo = e => {
-  const id = parseInt(e.target.parentNode.id);
-  store.dispatch(deleteToDo(id));
-};
+// const dispatchAddToDo = text => {
+//   store.dispatch(addToDo(text));
+// };
 
-const paintToDos = () => {
-  const toDos = store.getState();
-  ul.innerHTML = "";
-  toDos.forEach(toDo => {
-    const li = document.createElement("li");
-    const btn = document.createElement("button");
-    btn.innerText = "DEL";
-    btn.addEventListener("click", dispatchDeleteToDo);
-    li.id = toDo.id;
-    li.innerText = toDo.text;
-    li.appendChild(btn);
-    ul.appendChild(li);
-  });
-};
+// const dispatchDeleteToDo = e => {
+//   const id = parseInt(e.target.parentNode.id);
+//   store.dispatch(deleteToDo(id));
+// };
 
-store.subscribe(paintToDos);
+// const paintToDos = () => {
+//   const toDos = store.getState();
+//   ul.innerHTML = "";
+//   toDos.forEach(toDo => {
+//     const li = document.createElement("li");
+//     const btn = document.createElement("button");
+//     btn.innerText = "DEL";
+//     btn.addEventListener("click", dispatchDeleteToDo);
+//     li.id = toDo.id;
+//     li.innerText = toDo.text;
+//     li.appendChild(btn);
+//     ul.appendChild(li);
+//   });
+// };
 
-const onSubmit = e => {
-  e.preventDefault();
-  const toDo = input.value;
-  input.value = "";
-  dispatchAddToDo(toDo);
-};
+// store.subscribe(paintToDos);
 
-form.addEventListener("submit", onSubmit);
+// const onSubmit = e => {
+//   e.preventDefault();
+//   const toDo = input.value;
+//   input.value = "";
+//   dispatchAddToDo(toDo);
+// };
+
+// form.addEventListener("submit", onSubmit);
